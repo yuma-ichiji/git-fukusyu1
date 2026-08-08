5fbb25ef06df60b755b4217b5d84ada1fc3af249
yuma <yuuma3desu2014y@yahoo.co.jp>
2026-08-08 17:31:47 +0900

    あ

--- yu-za-kanri/pazis/pazis/02_riset.py
+++ yu-za-kanri/pazis/pazis/02_riset.py
@@ -6,7 +6,9 @@ if st.session_state.get("name"):
     st.write(f"名前: {st.session_state.get("name", "未設定")}")
     st.write(f"学年: {st.session_state.get("gakunen", "未設定")}")
     st.write(f"趣味: {", ".join(st.session_state.get("syumi", []))}")
+
     if st.button("すべての情報をリセット", type="primary"):
+        st.warning("本当にリセットしますか？")
         st.session_state.name = ""
         st.session_state.gakunen = ""
         st.session_state.syumi = []
