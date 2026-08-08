ddf3d484ce74e18de429cea87f6e229f24dc837e
yuma <yuuma3desu2014y@yahoo.co.jp>
2026-08-08 15:18:07 +0900

    名前記憶アプリの追加

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("hello world")
+
+
+import streamlit as st
+st.title("名前記憶アプリ")
+if "name" not in st.session_state:
+    st.session_state.name = ""
+name = st.text_input("あなたの名前を入力してください")
+if st.button("名前を記憶"):
+    st.session_state.name = name
+st.write(f"記憶している名前: {st.session_state.name}")
